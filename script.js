37728edcc9b4da771634cf583126eb0d23556621
GyanPrakash <gyanprakashdangi@gmail.com>
2022-09-15 12:34:01 +0530

    Added UT-1 and UT-2 Marks

--- script.js
+++ script.js
@@ -7,16 +7,48 @@ function myFunction(){
     document.getElementById("branch").innerHTML=branch;
     var seat=document.validform.txtSeat.value;
     document.getElementById("seat").innerHTML=seat;
-    var eb=document.validform.etxt.value;
-    document.getElementById("eb").innerHTML=eb;
-    var ip=document.validform.Iptxt.value;
-    document.getElementById("ip").innerHTML=ip;
-    var se=document.validform.Setxt.value;
-    document.getElementById("se").innerHTML=se;
-    var mt=document.validform.Mttxt.value;
-    document.getElementById("mt").innerHTML=mt;
-    var cns=document.validform.Cntxt.value;
-    document.getElementById("cns").innerHTML=cns;
+    // E-business
+    let ebu1=document.validform.ebu1.value;
+    document.getElementById("u1eb").innerHTML=ebu1;
+    let ebu2=document.validform.ebu2.value;
+    document.getElementById("u2eb").innerHTML=ebu2;
+    let ebfinal=document.validform.ebfinal.value;
+    document.getElementById("finaleb").innerHTML=ebfinal;
+
+    // Internet Programming
+    let Ipu1=document.validform.Ipu1.value;
+    document.getElementById("u1ip").innerHTML=Ipu1;
+    let Ipu2=document.validform.Ipu2.value;
+    document.getElementById("u2ip").innerHTML=Ipu2;
+    let Ipfinal=document.validform.Ipfinal.value;
+    document.getElementById("finalip").innerHTML=Ipfinal;
+
+    // Software Engineering
+    let Seu1=document.validform.Seu1.value;
+    document.getElementById("u1se").innerHTML=Seu1;
+    let Seu2=document.validform.Seu2.value;
+    document.getElementById("u2se").innerHTML=Seu2;
+    let Sefinal=document.validform.Sefinal.value;
+    document.getElementById("finalse").innerHTML=Sefinal;
+
+
+    // Advance Technology Mnagement
+    let Mtu1=document.validform.Mtu1.value;
+    document.getElementById("u1mt").innerHTML=Mtu1;
+    let Mtu2=document.validform.Mtu2.value;
+    document.getElementById("u2mt").innerHTML=Mtu2;
+    let Mtfinal=document.validform.Mtfinal.value;
+    document.getElementById("finalmt").innerHTML=Mtfinal;
+
+    // Computer Security Technology
+    let Cnu1=document.validform.Cnu1.value;
+    document.getElementById("u1cns").innerHTML=Cnu1;
+    let Cnu2=document.validform.Cnu2.value;
+    document.getElementById("u2cns").innerHTML=Cnu2;
+    let Cnfinal=document.validform.Cnfinal.value;
+    document.getElementById("finalcns").innerHTML=Cnfinal;
+
+
     // finding the total marks:
     let total=eval(parseFloat(document.validform.etxt.value)+parseFloat(document.validform.Iptxt.value)+parseFloat(document.validform.Setxt.value)+parseFloat(document.validform.Mttxt.value)+parseFloat(document.validform.Cntxt.value));
     document.getElementById("total").innerHTML=total;
